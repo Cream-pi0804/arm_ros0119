cac193f1b6f6242034a68fabc0950c2180e3f896
奶油糖派 <3310056144@qq.com>
2026-03-21 19:40:16 +0800

    0321添加launch

---
+++ src/arm/launch/arm.launch.py
@@ -0,0 +1,33 @@
+import launch
+import launch_ros
+
+def generate_launch_description():
+
+    motor_serial_node = launch_ros.actions.Node(
+        package='arm',
+        executable="motor_serial",
+        output='log',
+    )
+    solve_arm_ik_serves = launch_ros.actions.Node(
+        package='arm',
+        executable="solve_arm_ik",
+        output='log',
+    )
+    linear_move = launch_ros.actions.Node(
+        package='arm',
+        executable='linear_move',
+        output='log',
+    )
+    circular_move = launch_ros.actions.Node(
+        package='arm',
+        executable='circular_move',
+        output='log',
+    )
+   # 合成启动描述并返回
+    launch_description = launch.LaunchDescription([
+        motor_serial_node,
+        solve_arm_ik_serves,
+        linear_move,
+        circular_move
+    ])
+    return launch_description
